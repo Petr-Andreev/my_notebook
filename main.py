21cfa545ed2a0220f51302dbcc2e675a5f6dd66b
Petr Andreev <drakula2805@gmail.com>
2024-08-05 12:01:44 +0300

    Добавление gitignore, requirements Добавление подключение к БД

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from enum import Enum
-from typing import List
-
+import datetime
+from typing import List, Optional
 import uvicorn
 from fastapi import FastAPI
 from pydantic import BaseModel
@@ -18,8 +17,8 @@ users = [
 
 class User(BaseModel):
     id: int
-    role: str
     name: str
+    role: Optional[str] = None
 
 
 @app.get("/users/{user_id}", response_model=List[User])
@@ -27,34 +26,118 @@ def get_user(user_id: int):
     return [user for user in users if user.get('id') == user_id]
 
 
-tasks_list = [
-    {"id": 1, "user_id": 1, "title": "Покормить кота", "priority": "medium"},
-    {"id": 2, "user_id": 1, "title": "Почистить зубы", "priority": "low"},
-    {"id": 3, "user_id": 2, "title": "Накраситься", "priority": "low"},
-    {"id": 4, "user_id": 2, "title": "Подать документы на отпуск", "priority": "high"},
-    {"id": 5, "user_id": 3, "title": "Провести митинг", "priority": "medium"},
-    {"id": 6, "user_id": 3, "title": "Пообедать", "priority": "high"},
-    {"id": 7, "user_id": 3, "title": "Собрать статистику работы сотрудников", "priority": "high"},
-]
+# Модель для подзадачи
+class SubTask(BaseModel):
+    id: int
+    title: str
+    completed: bool
 
 
-class Priority(Enum):
-    low_priority = "low"
-    medium_priority = "medium"
-    high_priority = "high"
-
-
-class Task(BaseModel):
+# Модель для задач
+class STask(BaseModel):
     id: int
     user_id: int
     title: str
-    priority: Priority
+    priority: str
+    date_to: Optional[datetime.date] = None
+    subtasks: List[SubTask] = []  # Список подзадач, по умолчанию пуст
 
 
-@app.post('/tasks/create')
-def add_task(tasks: List[Task]):
-    tasks_list.extend(tasks)
-    return {'status': 200, 'data': tasks_list}
+# Пример данных задач
+tasks_list = [
+    STask(
+        id=1,
+        user_id=1,
+        title="Покормить кота",
+        priority="medium",
+        date_to=datetime.date(2024, 8, 6),
+        subtasks=[
+            SubTask(id=1, title="Купить корм", completed=False),
+            SubTask(id=2, title="Накормить кота", completed=False)
+        ]
+    ),
+    STask(
+        id=2,
+        user_id=1,
+        title="Почистить зубы",
+        priority="low",
+        date_to=datetime.date(2024, 8, 6),
+        subtasks=[]
+    ),
+    STask(
+        id=3,
+        user_id=2,
+        title="Накраситься",
+        priority="low",
+        date_to=datetime.date(2024, 8, 6),
+        subtasks=[
+            SubTask(id=1, title="Выбрать цвет", completed=False)
+        ]
+    ),
+    STask(
+        id=4,
+        user_id=2,
+        title="Подать документы на отпуск",
+        priority="high",
+        date_to=datetime.date(2024, 8, 20),
+        subtasks=[
+            SubTask(id=1, title="Заполнить документ", completed=False),
+            SubTask(id=2, title="Отправить документы на согласование", completed=False)
+        ]
+    ),
+    STask(
+        id=5,
+        user_id=3,
+        title="Провести митинг",
+        priority="low",
+        date_to=datetime.date(2024, 8, 10),
+        subtasks=[
+            SubTask(id=1, title="Созвониться с коллегами", completed=False)
+        ]
+    ),
+    STask(
+        id=6,
+        user_id=3,
+        title="Пообедать",
+        priority="low",
+        date_to=datetime.date(2024, 8, 10),
+        subtasks=[
+            SubTask(id=1, title="Купить еды в магазине", completed=False)
+        ]
+    ),
+    STask(
+        id=7,
+        user_id=3,
+        title="Собрать статистику работы сотрудников",
+        priority="low",
+        date_to=datetime.date(2024, 8, 29),
+        subtasks=[
+            SubTask(id=1, title="Опросить каждого работника", completed=False),
+            SubTask(id=2, title="Отфильтровать данные", completed=False),
+            SubTask(id=3, title="Составить отчет", completed=False)
+        ]
+    ),
+]
+
+
+@app.get("/tasks")
+def get_tasks(user_id: int, title: Optional[str] = None, priority: Optional[str] = None) -> List[STask]:
+    # Фильтрация задач по user_id
+    filtered_tasks = [task for task in tasks_list if task.user_id == user_id]
+
+    # Дополнительно фильтруем по названию задачи, чтобы посмотреть подзадачи этой задачи
+    if title:
+        filtered_tasks = [task for task in tasks_list if task.title == title]
+    # Если задан приоритет, фильтруем дополнительно
+    if priority:
+        filtered_tasks = [task for task in filtered_tasks if task.priority == priority]
+
+    return filtered_tasks
+
+
+@app.post("/add_task")
+def add_task(task: STask):
+    pass
 
 
 if __name__ == "__main__":
