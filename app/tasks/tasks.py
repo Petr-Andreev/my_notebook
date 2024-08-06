c10af2edb50f566a54d851ff72b07b276ae9b447
Petr Andreev <drakula2805@gmail.com>
2024-08-06 12:26:39 +0300

    Добавление новой таблицы - Users

--- app/tasks/tasks.py
+++ app/tasks/tasks.py
@@ -7,16 +7,17 @@ class Tasks(Base):
     __tablename__ = 'tasks'
 
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, nullable=False)
+    user_id = Column(ForeignKey('user.id'))  # Столбец внешнего ключа
     title = Column(String, nullable=False)
     priority = Column(String, default="medium")
     date_to = Column(DateTime)
     completed = Column(Boolean, default=False)
 
+
 class SubTasks(Base):
     __tablename__ = 'subtasks'
 
     id = Column(Integer, primary_key=True)
-    task_id = Column(Integer, ForeignKey('tasks.id'))  # Столбец внешнего ключа
+    task_id = Column(ForeignKey('tasks.id'))  # Столбец внешнего ключа
     title = Column(String, nullable=False)
     completed = Column(Boolean, default=False)
